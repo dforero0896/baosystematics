d7ebdc90928d3e3965b3fc9482ede78054f368bb
Daniel Forero Sanchez <dforero@cori05.nersc.gov>
2019-11-02 11:23:35 -0700

    Change fcfc.py code to work on nersc.

--- src/fcfc/fcfc.py
+++ src/fcfc/fcfc.py
@@ -2,17 +2,25 @@
 import sys
 import os
 import numpy as np
-if len(sys.argv) != 8:
-	sys.stderr.write('ERROR:\tUnexpected number of arguments.\nUSAGE:\tpython %s INPUT_PATH OUTPUT_PATH JOB_LIST_ID CONF_FILE R_MIN R_MAX OVERWRITE\n'%sys.argv[0])
+if len(sys.argv) == 6:
+	cat_type = 'gal'
+	overwrite = bool(int(sys.argv[5]))
+	r_min = None
+	r_max = r_min
+elif len(sys.argv) == 9:
+	cat_type = 'void'
+	r_min = sys.argv[5]
+	r_max = sys.argv[6]
+	ran_void_cat = sys.argv[7]
+	overwrite = bool(int(sys.argv[8]))
+else:
+	sys.stderr.write('ERROR:\tUnexpected number of arguments.\nUSAGE:\tpython %s INPUT_PATH OUTPUT_PATH JOB_LIST_ID CONF_FILE [R_MIN R_MAX VOID_RAN_DIR] OVERWRITE\n'%os.path.basename(sys.argv[0]))
 	sys.exit(1)
 this_dir = os.path.dirname(sys.argv[0])
 inPath = sys.argv[1]
 outPath = sys.argv[2]
 joblist_id = sys.argv[3]
 conf_file = sys.argv[4]
-r_min = sys.argv[5]
-r_max = sys.argv[6]
-overwrite = bool(int(sys.argv[7]))
 try:
 	(_, _, f) = next(os.walk(inPath))
 except StopIteration:
@@ -27,20 +35,15 @@ def get_region(fn):
 			return r;break
 	sys.stderr.write('ERROR: Could not identify region.\n')
 	sys.exit(1)
-cat_types=['gal', 'void']
-def get_cat_type(fn):
-	for t in cat_types:
-		if t in fn.lower():
-			return t;break
-	sys.stderr.write('ERROR: Could not identify catalog type=(gal, void) from CONF_FILE.\n')
-	sys.exit(1)
-cat_type = get_cat_type(conf_file)			
 regfirst = {r:True for r in regions}
-bash_script_name = os.path.join(this_dir,'fcfc_%s_%s.sh'%(cat_type,joblist_id))
-job_sub_name = os.path.join(this_dir, 'fcfcJob.sh')
+joblist_dir = os.path.join(this_dir, 'joblist')
+if not os.path.isdir(joblist_dir):
+	os.mkdir(joblist_dir)
+bash_script_name = os.path.join(joblist_dir,'fcfc_%s_%s.sh'%(cat_type,joblist_id))
+job_sub_name = os.path.join(joblist_dir, 'fcfcJob.sbatch')
 bash_script = open(bash_script_name, 'w')
 fdat = [fn for fn in f if '.dat.' in fn]
-for fileName in fdat:
+for fileno, fileName in enumerate(fdat):
 	print(fileName)
 	out_file = os.path.join(outPath,'TwoPCF_'+fileName)
 	if os.path.isfile(out_file) and not overwrite:
@@ -66,14 +69,12 @@ for fileName in fdat:
 		count_mode = 7
 	if not os.path.isfile(ran_cat_file):
 		continue
-	bash_script.write('srun /home/epfl/dforero/scripts/FCFC --conf=%s --data=%s --rand=%s --rand-convert=1 --data-convert=1 --count-mode=%s --dd=%s --dr=%s --rr=%s --output=%s --data-aux-min=%s --data-aux-max=%s --rand-aux-min=%s --rand-aux-max=%s\n'%(conf_file, dat_cat_file, ran_cat_file, count_mode, dd_file, dr_file, rr_file, out_file, r_min, r_max, r_min, r_max))
+	bash_script.write('srun -n 1 -c 64 /global/cscratch1/sd/dforero/baosystematics/bin/2pcf --conf=%s --data=%s --rand=%s --rand-convert=1 --data-convert=1 --count-mode=%s --dd=%s --dr=%s --rr=%s --output=%s --data-aux-min=%s --data-aux-max=%s --rand-aux-min=%s --rand-aux-max=%s\n'%(conf_file, dat_cat_file, ran_cat_file, count_mode, dd_file, dr_file, rr_file, out_file, r_min, r_max, r_min, r_max))
 bash_script.close()
-tmp_header = os.path.join(this_dir, 'fcfcJob_header.sh')
-os.system('head -18 %s > %s'%(job_sub_name, tmp_header))
-os.system('cat %s > %s'%(tmp_header, job_sub_name))
-os.system('cat %s >> %s'%(bash_script_name, job_sub_name))
-os.system('rm %s'%tmp_header)#TODO: Evaluate the necesity of having copies of submission scripts for different steps
-os.system('mkdir %s'%os.path.join(outPath, 'DD_files'))
-os.system('mkdir %s'%os.path.join(outPath, 'DR_files'))
-os.system('mkdir %s'%os.path.join(outPath, 'RR_files'))
-
+dd_dir = os.path.join(outPath, 'DD_files')
+dr_dir = os.path.join(outPath, 'DR_files')
+rr_dir = os.path.join(outPath, 'RR_files')
+paircount_dirs = [dd_dir, dr_dir, rr_dir]
+for dir_ in paircount_dirs:
+	if not os.path.isdir(dir_):
+		os.mkdir(dir_)
