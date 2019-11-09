8fa31d2ac57d639453e808e33d61f75ce7215c26
Daniel Forero Sanchez <dforero@cori05.nersc.gov>
2019-11-09 07:34:54 -0800

    2pcf tests on allsyst and nosyst.

--- src/apply_systematics/tests/test_apply_systematics.sh
+++ src/apply_systematics/tests/test_apply_systematics.sh
@@ -2,8 +2,8 @@
 #test_apply_systematics.sh
 module load python
 WORKDIR=/global/cscratch1/sd/dforero/baosystematics/
-COMMAND_A="python $WORKDIR/src/apply_systematics/apply_systematics.py $WORKDIR/data/init_test_v7/mock/EZ_ELG_clustering_NGC_v7.dat.541.fits $WORKDIR/results/init_test_v7/mocks_gal_rdz/EZ_ELG_clustering_NGC_v7.dat.126.ascii NOZ SYSTOT CP"
-COMMAND_B="python $WORKDIR/src/apply_systematics/apply_systematics.py $WORKDIR/data/init_test_v7/mock/EZ_ELG_clustering_NGC_v7.dat.541.fits $WORKDIR/results/init_test_v7/mocks_gal_rdz/EZ_ELG_clustering_NGC_v7.dat.126.ascii SYSTOT"
+COMMAND_A="python $WORKDIR/src/apply_systematics/apply_systematics.py $WORKDIR/data/init_test_v7/mock/EZ_ELG_clustering_NGC_v7.dat.541.fits $WORKDIR/results/init_test_v7/mocks_gal_rdz/EZ_ELG_clustering_NGC_v7.dat.541.ascii NOZ SYSTOT CP"
+COMMAND_B="python $WORKDIR/src/apply_systematics/apply_systematics.py $WORKDIR/data/init_test_v7/mock/EZ_ELG_clustering_NGC_v7.dat.541.fits $WORKDIR/results/init_test_v7/mocks_gal_rdz/EZ_ELG_clustering_NGC_v7.dat.541.ascii SYSTOT"
 $COMMAND_A
 echo #######################################################################
 echo $COMMAND_A
--- src/baofit/baofit.py
+++ src/baofit/baofit.py
@@ -1,22 +1,20 @@
 #!/usr/bin/env python3
-import numpy as np
-import matplotlib.pyplot as plt
 import os
 import sys
 if len(sys.argv)!=5:
 	sys.exit('ERROR:\tUnexpected number of arguments.\nUSAGE:\t{0} MOCK_DIR INPUT_2PCF OUT_FILE BEST_FIT_FILE'.format(sys.argv[0]))
-mockPath = sys.argv[0]
+mockPath = sys.argv[1]
+input2PCF = sys.argv[2]
+outFile = sys.argv[3]
+bestFitFile = sys.argv[4]
 def saveMockList(path, name='mocklist.dat'):
 	mockList = os.listdir(path)
 	mockFile = open(name, 'w')
 	for m in mockList:
-		mockFile.write('%s%s\n'%(path, m))
+		mockFile.write(os.path.join(path, m+'\n'))
 	mockFile.close()
 mockFile='mocklist.dat'
 saveMockList(mockPath, mockFile)
-input2PCF = '/home/epfl/dforero/comb2pcf/data_comb_2pcf/TwoPCF_eBOSS_ELG_clustering_CGV_v4.dat.ascii'
-outFile = 'ELG_baofit.dat'
-bestFitFile = 'ELG_bestfit.dat'
 #os.system('/home/epfl/zhaoc/work/codes/BAOfit/code_nest/bestfit -c baofit.conf -i %s -m %s -o %s -b %s'%(input2PCF, mockFile, outFile, bestFitFile))
 
 os.system('/home/epfl/zhaoc/work/codes/BAOfit/code_nest/BAOfit -c baofit.conf -i %s -m %s -o %s -b %s'%(input2PCF, mockFile, outFile, bestFitFile))
