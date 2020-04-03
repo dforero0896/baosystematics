Wrong nearest approach to void weighting

src/2pcf/2pcf.py
@@ -4,13 +4,15 @@ def rr_analytic(bin_low_bound, bin_high_bound, box_size):
     volume = 4 * np.pi * (bin_high_bound**3 - bin_low_bound**3) / 3
     normed_volume = volume / box_size **3
     return normed_volume
-def tpcf(dd):
+def tpcf(dd, box_size=2500):
     """ Compute the analytic two-point correlation function from the DD pair-counts.
 
     dd (ndarray): left_bin_edge, right_bin_edge, mono_pair_count, mono_normed_pair_count\
-                  quad_pair_count, quad_normed_pair_count, hexa_pair_count, hexa_normed_pair_count."""
+                  quad_pair_count, quad_normed_pair_count, hexa_pair_count, hexa_normed_pair_count.
+    box_size (float, optional): Size of the side of the box to consider, in Mpc.
+		Default: box_size = 2500"""
     
-    rr = rr_analytic(dd[:,0], dd[:,1], 2500)
+    rr = rr_analytic(dd[:,0], dd[:,1], box_size)
     delta_s = dd[:,1] - dd[:,0]
     s = dd[:,0] + 0.5 * delta_s
     monopole = ( dd[:,3] / rr ) - 1

src/simulate_systematics/comp_mask_catalog.py
@@ -7,17 +7,24 @@ from mask_comp_func import mask_with_function
 from params import *
 
 
-def comp_mask_catalog(fn, odir, sigma_noise=0.2, function=parabola, cmin=0.8, names = ['x', 'y', 'z'], N_grid=2500, noise_sampler=noise_sampler, rmin=16, rmax=50, cat_type='mock'):
+def comp_mask_catalog(fn, odir, sigma_noise=0.2, function=parabola,\
+			cmin=0.8, names = ['x', 'y', 'z'], N_grid=2500, \
+			noise_sampler=noise_sampler, rmin=16, rmax=50, \
+			cat_type='mock'):
     print(f"==> Using function {function.__name__}")
     print(f"==> Using min. completeness {cmin}")
     complete_command='module load spack/default  gcc/5.4.0 boost\n'
     # Create data catalog with mask
     fn_base = os.path.basename(fn)
-    oname_gal = fn_base.replace('.dat', f".{function.__name__}.sigma{sigma_noise}.dat")
-    oname_gal_noise = os.path.join(odir, f"noise/{function.__name__}_{cmin}/{cat_type}s_gal_xyz/{oname_gal}")
-    oname_gal_smooth = os.path.join(odir, f"smooth/{function.__name__}_{cmin}/{cat_type}s_gal_xyz/{oname_gal.replace('sigma'+str(sigma_noise), 'sigma0.0')}")
+    oname_gal = fn_base.replace('.dat',\
+	 f".{function.__name__}.sigma{sigma_noise}.dat")
+    oname_gal_noise = os.path.join(odir,\
+	 f"noise/{function.__name__}_{cmin}/{cat_type}s_gal_xyz/{oname_gal}")
+    oname_gal_smooth = os.path.join(odir,\
+	 f"smooth/{function.__name__}_{cmin}/{cat_type}s_gal_xyz/{oname_gal.replace('sigma'+str(sigma_noise), 'sigma0.0')}")
     onames = [oname_gal_smooth, oname_gal_noise]
-    obases = [os.path.join(odir, s,f"{function.__name__}_{cmin}") for s in ['smooth', 'noise']]
+    obases = [os.path.join(odir, \
+	 s,f"{function.__name__}_{cmin}") for s in ['smooth', 'noise']]
     conf_file_gal = WORKDIR+"/src/fcfc_box/fcfc_box_count_%s.conf"%SPACE
     conf_file_void = WORKDIR+"/src/fcfc_box/fcfc_box_void_count_%s.conf"%SPACE
     joblist=open('joblist.sh', 'w')
@@ -25,7 +32,8 @@ def comp_mask_catalog(fn, odir, sigma_noise=0.2, function=parabola, cmin=0.8, na
         write_void=False #So they are not recomputed in reruns
         if not os.path.exists(on):
             write_void = True #In case of error, in galaxies, overwrite voids too.
-            data = pd.read_csv(fn, delim_whitespace = True, usecols=(0, 1, 2), names = names)
+            data = pd.read_csv(fn, delim_whitespace = True, \
+				usecols=(0, 1, 2), names = names)
             os.makedirs(os.path.dirname(on), exist_ok=True)
             print(f"Creating {on}")
             mask_function = lambda y, x: function(y, x, N_grid, cmin)
@@ -33,26 +41,42 @@ def comp_mask_catalog(fn, odir, sigma_noise=0.2, function=parabola, cmin=0.8, na
                 nsampler = lambda n_samples: noise_sampler(sigma_noise, n_samples)
             else:
                 nsampler=None
-            masked_dat = mask_with_function(data, mask_function, noise=bool(i), box_size=2500, N_grid=N_grid, sigma_noise=sigma_noise, cmin=0, noise_sampler=nsampler)
+            masked_dat = mask_with_function(data, mask_function,\
+					 noise=bool(i), box_size=2500,\
+					 N_grid=N_grid, sigma_noise=sigma_noise,\
+					 cmin=0, noise_sampler=nsampler)
             masked_dat.to_csv(on, sep = ' ', header=False, index=False)
 
-        oname_void = os.path.join(obases[i], f"{cat_type}s_void_xyz", os.path.basename(on).replace('.dat', f".VOID.dat"))
-        os.makedirs(os.path.dirname(oname_void), exist_ok=True)
-        dive_command = f"{RUN_DIVE} {on} {oname_void} {box_size} 0 999\n"
-        if not os.path.exists(oname_void) or write_void:
+        #print(f"==> WARNING: Saving voids in 'nearest' directory")
+        oname_void = [os.path.join(obases[i],\
+			 f"{cat_type}s_void_xyz",\
+			 os.path.basename(on).replace('.dat', f".VOID.dat")), \
+			 os.path.join(obases[i], 
+			 f"{cat_type}s_void_xyz_wt",\
+			 os.path.basename(on).replace('.dat', f".VOID.dat"))]
+        [os.makedirs(os.path.dirname(oname_void[i]), exist_ok=True) for i in range(2)]
+        dive_command = f"{RUN_DIVE} {on} {oname_void[0]} {box_size} 0 999\n"
+        if not os.path.exists(oname_void[0]) or write_void:
             joblist.write(dive_command)
             complete_command+=dive_command
 
         # Define files for fcfc
-        tpcf_gal_dirs = [obases[i]+f"/tpcf_gal_{cat_type}_nowt", obases[i]+f"/tpcf_gal_{cat_type}"]
-        tpcf_void_dirs = [obases[i]+f"/tpcf_void_{cat_type}_nowt_R-{rmin}-{rmax}", obases[i]+f"/tpcf_void_{cat_type}_R-{rmin}-{rmax}"]
-        tpcf_xgv_dirs = [obases[i]+f"/tpcf_xgv_{cat_type}_vnw_gnw_R-{rmin}-{rmax}", obases[i]+f"/tpcf_xgv_{cat_type}_vnw_gw_R-{rmin}-{rmax}", obases[i]+f"/tpcf_xgv_{cat_type}_vw_gnw_R-{rmin}-{rmax}", obases[i]+f"/tpcf_xgv_{cat_type}_vw_gw_R-{rmin}-{rmax}"]
+        tpcf_gal_dirs = [obases[i]+f"/tpcf_gal_{cat_type}_nowt", \
+			obases[i]+f"/tpcf_gal_{cat_type}"]
+        tpcf_void_dirs = [obases[i]+f"/tpcf_void_{cat_type}_nowt_R-{rmin}-{rmax}", \
+			obases[i]+f"/tpcf_void_{cat_type}_R-{rmin}-{rmax}"]
+        tpcf_xgv_dirs = [obases[i]+f"/tpcf_xgv_{cat_type}_vnw_gnw_R-{rmin}-{rmax}", \
+			obases[i]+f"/tpcf_xgv_{cat_type}_vnw_gw_R-{rmin}-{rmax}", \
+			obases[i]+f"/tpcf_xgv_{cat_type}_vw_gnw_R-{rmin}-{rmax}", \
+			obases[i]+f"/tpcf_xgv_{cat_type}_vw_gw_R-{rmin}-{rmax}"]
         data_wt_cols_gal= [0, 4]
         data_wt_cols_void= [0, 5]
         for weight in [0,1]:
             os.makedirs(tpcf_gal_dirs[weight]+"/DD_files", exist_ok=True)
-            dd_file_gal = os.path.join(tpcf_gal_dirs[weight],"DD_files","DD_"+os.path.basename(on))
-            out_file_gal = os.path.join(tpcf_gal_dirs[weight], "TwoPCF_"+os.path.basename(on))
+            dd_file_gal = os.path.join(tpcf_gal_dirs[weight],"DD_files",\
+					"DD_"+os.path.basename(on))
+            out_file_gal = os.path.join(tpcf_gal_dirs[weight],\
+					 "TwoPCF_"+os.path.basename(on))
             data_wt_col=data_wt_cols_gal[weight]
             if not os.path.exists(dd_file_gal):
                 count_mode=1
@@ -67,38 +91,47 @@ def comp_mask_catalog(fn, odir, sigma_noise=0.2, function=parabola, cmin=0.8, na
             
             
             # For voids
-            if weight==0: #Remove this condition to implement void weights
-                os.makedirs(tpcf_void_dirs[weight]+"/DD_files", exist_ok=True)
-                dd_file_void = os.path.join(tpcf_void_dirs[weight],"DD_files","DD_"+os.path.basename(oname_void).replace('.dat', f".R-{rmin}-{rmax}.dat"))
-                out_file_void = os.path.join(tpcf_void_dirs[weight], "TwoPCF_"+os.path.basename(oname_void).replace('.dat', f".R-{rmin}-{rmax}.dat"))
-                data_wt_col=data_wt_cols_void[weight]
-                if not os.path.exists(dd_file_void):
-                    count_mode=1
+#            if weight==0: #Remove this condition to implement void weights
+            if not os.path.isfile(oname_void[weight]): 
+                continue; print(f"==> File {oname_void[weight]} does not exist.")
+            os.makedirs(tpcf_void_dirs[weight]+"/DD_files", exist_ok=True)
+            dd_file_void = os.path.join(tpcf_void_dirs[weight],"DD_files",\
+		"DD_"+os.path.basename(oname_void[weight]).replace('.dat',\
+							f".R-{rmin}-{rmax}.dat"))
+            out_file_void = os.path.join(tpcf_void_dirs[weight],\
+		 "TwoPCF_"+os.path.basename(oname_void[weight]).replace('.dat',\
+							f".R-{rmin}-{rmax}.dat"))
+            data_wt_col=data_wt_cols_void[weight]
+            if not os.path.exists(dd_file_void):
+                count_mode=1
+                ncores = NCORES
+                fcfc_void_command = f"srun -n 1 -c {ncores} {RUN_FCFC} --conf={conf_file_void} --data={oname_void[weight]} --count-mode={count_mode} --dd={dd_file_void} --output={out_file_void} --data-wt-col={data_wt_col} --data-aux-col=4 --data-aux-min={rmin} --data-aux-max={rmax} --cf-mode=3\n"
+            else:
+                ncores=1
+                count_mode=0
+                fcfc_void_command = f"{WORKDIR}/bin/2pcf.py {dd_file_void} {out_file_void}\n"
+            joblist.write(fcfc_void_command)    
+            complete_command+=fcfc_void_command
+             # Compute cross-terms
+            for weight_gal in [0,1]:
+                id_ = np.ravel_multi_index((weight, weight_gal), (2,2))
+                os.makedirs(tpcf_xgv_dirs[id_]+"/DD_files", exist_ok=True)
+                dgdv_file = os.path.join(tpcf_xgv_dirs[id_], "DD_files", \
+			"DD_"+os.path.basename(on).replace('.dat', \
+						f".CROSS.R_{rmin}-{rmax}.dat"))
+                out_file_xgv = os.path.join(tpcf_xgv_dirs[id_],\
+			"TwoPCF_"+os.path.basename(on).replace('.dat', \
+						f".CROSS.R_{rmin}-{rmax}.dat"))
+                if not os.path.exists(dgdv_file):
+                    count_mode=2
                     ncores = NCORES
-                    fcfc_void_command = f"srun -n 1 -c {ncores} {RUN_FCFC} --conf={conf_file_void} --data={oname_void} --count-mode={count_mode} --dd={dd_file_void} --output={out_file_void} --data-wt-col={data_wt_col} --data-aux-col=4 --data-aux-min={rmin} --data-aux-max={rmax} --cf-mode=3\n"
+                    fcfc_cross_command = f"srun -n 1 -c {ncores} {RUN_FCFC} --conf={conf_file_void} --rand={on} --data={oname_void[weight]} --count-mode={count_mode} --dr={dgdv_file} --output={out_file_xgv} --data-wt-col={data_wt_cols_void[weight]} --rand-wt-col={data_wt_cols_gal[weight_gal]} --data-aux-col=4 --data-aux-min={rmin} --data-aux-max={rmax} --cf-mode=0 && {WORKDIR}/bin/2pcf.py {dgdv_file} {out_file_xgv}\n"
                 else:
                     ncores=1
-                    count_mode=0
-                    fcfc_void_command = f"{WORKDIR}/bin/2pcf.py {dd_file_void} {out_file_void}\n"
-                joblist.write(fcfc_void_command)    
-                complete_command+=fcfc_void_command
-
-                # Compute cross-terms
-                for weight_gal in [0,1]:
-                    id_ = np.ravel_multi_index((weight, weight_gal), (2,2))
-                    os.makedirs(tpcf_xgv_dirs[id_]+"/DD_files", exist_ok=True)
-                    dgdv_file = os.path.join(tpcf_xgv_dirs[id_], "DD_files", "DD_"+os.path.basename(on).replace('.dat', f".CROSS.R_{rmin}-{rmax}.dat"))
-                    out_file_xgv = os.path.join(tpcf_xgv_dirs[id_], "TwoPCF_"+os.path.basename(on).replace('.dat', f".CROSS.R_{rmin}-{rmax}.dat"))
-                    if not os.path.exists(dgdv_file):
-                        count_mode=2
-                        ncores = NCORES
-                        fcfc_cross_command = f"srun -n 1 -c {ncores} {RUN_FCFC} --conf={conf_file_void} --rand={on} --data={oname_void} --count-mode={count_mode} --dr={dgdv_file} --output={out_file_xgv} --data-wt-col={data_wt_cols_void[weight]} --rand-wt-col={data_wt_cols_gal[weight_gal]} --data-aux-col=4 --data-aux-min={rmin} --data-aux-max={rmax} --cf-mode=0 && {WORKDIR}/bin/2pcf.py {dgdv_file} {out_file_xgv}\n"
-                    else:
-                        ncores=1
-                        fcfc_cross_command = f"{WORKDIR}/bin/2pcf.py {dgdv_file} {out_file_xgv}\n"
-                    joblist.write(fcfc_cross_command)    
-                    complete_command+=fcfc_cross_command
-          
+                    fcfc_cross_command = f"{WORKDIR}/bin/2pcf.py {dgdv_file} {out_file_xgv}\n"
+                joblist.write(fcfc_cross_command)    
+                complete_command+=fcfc_cross_command
+      
 
     joblist.close()
     return complete_command
@@ -121,7 +154,10 @@ if __name__ == '__main__':
     joblist = open(f"joblists/jobs_{SPACE}_{FUNCTION.__name__}_{cmin_map}_{iproc}.sh", 'w')
     for f in filenames_split[iproc]:
         odir = os.path.abspath(os.path.dirname(f)+'/../..')
-        command=comp_mask_catalog(f, odir, noise_sampler=noise_sampler, function = FUNCTION, cmin = cmin_map, N_grid = 2500, rmin = 16, rmax = 50, sigma_noise=0.2)
+        command=comp_mask_catalog(f, odir, noise_sampler=noise_sampler, \
+				function = FUNCTION, cmin = cmin_map, \
+				N_grid = 2500, rmin = 16, rmax = 50, \
+				sigma_noise=0.2)
         joblist.write(command)
     joblist.close()
     MPI.Finalize()

src/simulate_systematics/mask_comp_func.py
@@ -17,7 +17,7 @@ def mask_with_function(data, comp_mesh, seed=2, noise=True, box_size=2500, N_gri
       print("Using non-default noise sampler.")
       comp += noise_sampler(len(data)) # noise sampler must accept number of samples to graw as parameter.
     else:
-      comp += np.random.normal(0, sigma_noise, len(data)) #add noise to completeness
+      comp -= np.abs(np.random.normal(0, sigma_noise, len(data))) #add noise to completeness
   data['comp']=np.clip(comp, cmin, 1)
   data_ret = data[data['comp'] > rand]
   return data_ret

src/simulate_systematics/params.py
@@ -3,9 +3,9 @@ import pandas as pd
 import numpy as np
 from mask_comp_func import mask_with_function
 NCORES = 16
-BOX = "5"
-NMOCKS = 100
-SPACE = "redshift"
+BOX = "1"
+NMOCKS = 500
+SPACE = "real"
 # Define relevant paths
 WORKDIR = "/home/epfl/dforero/scratch/projects/baosystematics"
 RESULTS = WORKDIR+"/patchy_results"
@@ -30,7 +30,8 @@ def noise_sampler(sigma_noise, n_samples):
 
 # Define masking functions to be used
 def parabola(y, x, N_grid, Cmin):
-  return -(2 * (1-Cmin) / N_grid**2 ) * ((x- 0.5 * N_grid)**2 + (y- 0.5 * N_grid)**2) + 1
+  return -(2 * (1-Cmin) / N_grid**2 ) * ((x- 0.5 * N_grid)**2 +\
+		 (y- 0.5 * N_grid)**2) + 1
 def xplane(y, x, N_grid, Cmin):
   return ( (1-Cmin) / N_grid ) * x + Cmin
 def flat(y, x, N_grid, Cmin):
@@ -38,5 +39,13 @@ def flat(y, x, N_grid, Cmin):
 def parabola_off(y, x, N_grid, Cmin):
   return -((1-Cmin)/(2*N_grid**2)) * (x**2 + y**2) + 1
 funclist = [parabola, xplane, flat, parabola_off]
+
 # Define which function is actually being used
-FUNCTION = flat
+FUNCTION = parabola
+
+# Define radius and distance bins to sample denstities
+radius_bins = np.append(np.linspace(0, 21, 22), [25, 30, 50])
+radius_bin_widths = radius_bins[1:] - radius_bins[:-1]
+xy_bins = 256
+xedges = np.linspace(0, box_size, xy_bins+1)
+yedges = np.linspace(0, box_size, xy_bins+1)

src/simulate_systematics/plot_void_catalog.py
@@ -9,9 +9,10 @@ import sys
 import os
 from params import *
 names = ['x', 'y', 'z', 'r']
-def get_histogram(cat, rmin, rmax, bins=20, weights = None):
+def get_histogram(cat, rmin, rmax, bins=20, weights = False):
   cat_bin = cat[(cat['r'] > rmin ) & (cat['r'] < rmax)]
-  H, xedges, yedges = np.histogram2d(cat_bin['x'].values, cat_bin['y'].values, density=False, bins=(bins, bins), range = [[0, box_size], [0, box_size]], weights = weights)
+  if weights: weights = cat_bin['w']
+  H, xedges, yedges = np.histogram2d(cat_bin['x'].values, cat_bin['y'].values, density=False, bins=bins, range = [[0, box_size], [0, box_size]], weights = weights)
   return H, xedges, yedges
 if __name__ == '__main__':
   if len(sys.argv) < 2:
@@ -27,8 +28,6 @@ if __name__ == '__main__':
   except:
     odir = '.'
   out_fn = f"{odir}/void_density_map.npy"
-  radius_bins = np.append(np.linspace(0, 21, 22), [25, 30, 50])
-  xy_bins = 256
 
   # Set values for subplot
   side_x = int(np.sqrt(len(radius_bins)-1))
@@ -61,13 +60,17 @@ if __name__ == '__main__':
   # Create send buffers
   cat_arrays = np.empty((len(radius_bins)-1, xy_bins, xy_bins, len(catalogs_split[iproc])), dtype=np.float32)
   # Bottleneck
-  if not os.path.exists(out_fn):
+  if True:#not os.path.exists(out_fn):
     if iproc==0: print(f"==> Computing histogram with {nproc} processes.")
     for nmock, cat_fn in enumerate(catalogs_split[iproc]):
       print(f"==> MPI process {iproc}: {cat_fn}")
-      cat = pd.read_csv(cat_fn, delim_whitespace=True, names=['x', 'y', 'r'], usecols=(0,1,3), dtype=np.float32)
+      labels = radius_bins[:-1] + 0.5 * radius_bin_widths
+      cat = pd.read_csv(cat_fn, delim_whitespace=True, names=['x', 'y', 'r', 'w'], usecols=(0,1,3,4), dtype=np.float32)
+      weights = cat['w']
+      if weights.isna().any(): weights=None
+      cat.set_index('r')
       for i, lbound in enumerate(radius_bins[:-1]):
-        hist, xedges, yedges = get_histogram(cat, radius_bins[i], radius_bins[i+1], bins = xy_bins)
+        hist, xedges, yedges = get_histogram(cat, radius_bins[i], radius_bins[i+1], bins = [xedges, yedges], weights=not None)
         cat_arrays[i, :, :, nmock] = hist
     pmean = cat_arrays.mean(axis=3)
     print(f"==> MPI process {iproc} waiting for the others.")
@@ -78,11 +81,13 @@ if __name__ == '__main__':
     if iproc==0:
       histograms = np.average(cat_array_all, axis=0, weights = hist_weights)
       print(f"==> MPI process 0 saving file.")
+      if weights is not None: out_fn = out_fn.replace('.npy', '_wt.npy')
       np.save(out_fn, histograms)    
   else:
     if iproc==0:
       print(f"==> Loading data from {out_fn}.")
       histograms = np.load(out_fn)
+      weights=None
       print(f"==> Done")
   if iproc==0:
     xedges = np.linspace(0, box_size, xy_bins+1)
@@ -107,6 +112,9 @@ if __name__ == '__main__':
     plt.setp([a.get_yticklabels() for a in ax.ravel()], visible=False)
     plt.setp([a.get_xticklabels() for a in ax.ravel()], visible=False)
   #  plt.show()
-    fig.savefig(f"{odir}/all_void_density_maps.pdf", dpi=300)
-    print(f"Saved plot in {odir}.")
+    oname = f"{odir}/all_void_density_maps.pdf"
+    if weights is not None: oname = oname.replace('.pdf', '_wt.pdf')
+   
+    fig.savefig(oname, dpi=300)
+    print(f"Saved plot in {oname}")
   MPI.Finalize()

src/void_weights/apply_void_weights.py
@@ -8,37 +8,144 @@ sys.path.append(f"{WORKDIR}/src/simulate_systematics")
 from params import *
 from plot_void_catalog import get_histogram
 import matplotlib.pyplot as plt
+from scipy.interpolate import NearestNDInterpolator, RegularGridInterpolator
 
-def add_void_weights(void_cat, comp_function):
-    '''Apply void weights as a function of galaxy weights.
-
-    void_cat (pandas DataFrame): Void catalog to add weights to. Columns x, y, z, r
-    comp_function (callable): Void weight as a function of void radius and tracer position. w_v = function(y, x, r)'''
-    
-    void_cat['w'] = comp_function(void_cat['y'], void_cat['x'], void_cat['r'])
-    return void_cat
-
-def get_void_weights(void_cat, coeffs, comp_function):
-    c0 = np.interp(void_cat['r'], coeffs[:,0], coeffs[:,1])
-    c1 = np.interp(void_cat['r'], coeffs[:,0], coeffs[:,2])
-    c2 = np.interp(void_cat['r'], coeffs[:,0], coeffs[:,3])
-    gal_wt = (comp_function(void_cat['y'], void_cat['x']))**(-1)
-    return c0 + c1 * gal_wt + c2 * gal_wt**2
-if __name__ == '__main__':
-    if len(sys.argv) < 4:
-        sys.exit(f"ERROR: Unexpected number of arguments.\nUSAGE: {sys.argv[0]} VOID_WT_COEFF VOID_CAT OUT_VOID_CAT")
-    void_wt_coeff_fn = sys.argv[1]
-    void_cat_fn = sys.argv[2]
-    out_cat_fn = sys.argv[3]
-    os.makedirs(os.path.dirname(out_cat_fn), exist_ok=True)
+def add_void_weights_interp(void_cat_fn, void_wt_coeff_fn, comp_function, 
+			out_cat_fn = None,
+			save=True):
+    """Apply void weights as a function of galaxy weights.
+   """ 
+    if out_cat_fn is None: out_cat_fn = os.path.dirname(void_cat_fn)+\
+					"_wt/"+os.path.basename(void_cat_fn)
     # Import data
     print(f"==> Reading {void_cat_fn}")
-    void_cat = pd.read_csv(void_cat_fn, delim_whitespace = True, usecols=(0, 1, 2, 3), names = ['x', 'y', 'z', 'r'], dtype=np.float32)
+    void_cat = pd.read_csv(void_cat_fn, delim_whitespace = True,\
+			 usecols=(0, 1, 2, 3), names = ['x', 'y', 'z', 'r'],\
+			 dtype=np.float32)
     void_wt_coeff = np.loadtxt(void_wt_coeff_fn, dtype = np.float32) 
     print(f"==> Computing void weights")
-    void_wt = get_void_weights(void_cat, void_wt_coeff, lambda y, x: flat(y, x, N_grid=2500, Cmin = 0.8))
+    void_wt = get_void_weights_interp(void_cat, void_wt_coeff, comp_function)
     void_cat['w'] = void_wt
-    print(f"==> Saving catalog to {out_cat_fn}")
-    void_cat.to_csv(out_cat_fn, index=False, header = False, sep = ' ', float_format='%.5f')
-    print("==> Done")
+    if save:
+        print(f"==> Saving catalog to {out_cat_fn}")
+        os.makedirs(os.path.dirname(out_cat_fn), exist_ok=True)
+        void_cat.to_csv(out_cat_fn, index=False, header = False, sep = ' ',\
+			 float_format='%.5f')
+        print("==> Done")
+    return void_cat
+
+def add_void_weights(void_cat_fn, get_weights_func, out_cat_fn = None,
+			 out_dir_suffix='_wt', save=True, **kwargs):
+    """Apply void weights as a function of galaxy weights.
+   """ 
+    
+    if out_cat_fn is None: out_cat_fn = os.path.dirname(void_cat_fn)+\
+				out_dir_suffix+"/"+os.path.basename(void_cat_fn)
+    # Import data
+    print(f"==> Reading {void_cat_fn}")
+    void_cat = pd.read_csv(void_cat_fn, delim_whitespace = True,\
+			 usecols=(0, 1, 2, 3), names = ['x', 'y', 'z', 'r'],\
+			 dtype=np.float32)
+    print(f"==> Computing void weights with {get_weights_func.__name__}")
+    void_wt = get_weights_func(void_cat['r'], void_cat['x'], void_cat['y'], 
+				**kwargs)
+    void_cat['w'] = void_wt
+    if save:
+        print(f"==> Saving catalog to {out_cat_fn}")
+        os.makedirs(os.path.dirname(out_cat_fn), exist_ok=True)
+        void_cat.to_csv(out_cat_fn, index=False, header = False, sep = ' ',\
+			 float_format='%.5f')
+        print("==> Done")
+    return void_cat
+
+def batch_add_void_weights(void_cat_fn_list, get_weights_func, 
+				out_dir_suffix = '_wt', **kwargs):
+    
+
+    from mpi4py import MPI
+    comm = MPI.COMM_WORLD
+    size = comm.Get_size()
+    rank = comm.Get_rank()
+    if rank ==0 : print(f"==> Using {size} processes.")
+    void_cat_chunk_fn = np.array_split(void_cat_fn_list, size)[rank]
+    print(f"==> MPI process {rank} has {len(void_cat_chunk_fn)} jobs allocated.")
+    for void_cat_fn in void_cat_chunk_fn:
+        add_void_weights(void_cat_fn, get_weights_func, 
+			out_dir_suffix = out_dir_suffix,
+			out_cat_fn = None, save=True, **kwargs)
+    print(f"==> MPI process {rank} finished.")
+    MPI.Finalize()
+
+def get_void_weights_interp(r, x, y, coeffs, comp_function):
+    """Compute void weights from galaxy completeness(= 1/galaxy_weight)
+
+    """
+
+    c0 = np.interp(r, coeffs[:,0], coeffs[:,1])
+    c1 = np.interp(r, coeffs[:,0], coeffs[:,2])
+    c2 = np.interp(r, coeffs[:,0], coeffs[:,3])
+    gal_wt = (comp_function(y, x))**(-1)
+    return c0 + c1 * gal_wt + c2 * gal_wt**2
+
+def edges_to_centers(edges):
+    widths = edges[1:] - edges[:-1]
+    centers = edges[:-1] + 0.5 * widths
+    return centers, widths
+def get_known_void_weight_matrix(raw_void_dist_fn, void_dist_fn, 
+				radius_bins = radius_bins,
+				xedges = xedges, yedges=yedges):
+    """Get void weight matrix from R-binned arrays of void number 
+	density/void completeness
+
+    """
+    raw_void_dist = np.load(raw_void_dist_fn)
+    void_dist = np.load(void_dist_fn)
+    rcenters,_ = edges_to_centers(radius_bins)
+    xcenters,_ = edges_to_centers(xedges)
+    ycenters,_ = edges_to_centers(yedges)
+    R, X, Y = np.meshgrid(rcenters, xcenters, ycenters)
+    R = R.ravel(); X = X.ravel(); Y = Y.ravel()
+    
+    assert void_dist.shape[0] == rcenters.shape[0]    
+    assert raw_void_dist.shape[0] == rcenters.shape[0]    
+    
+    void_weight_matrix = raw_void_dist / void_dist
+    # Nearest interpolation returns closest value.
+    void_weight_interpolator = NearestNDInterpolator((R, X, Y),
+					 void_weight_matrix.ravel())
+
+    return lambda r, x, y: void_weight_interpolator((r, x, y)), void_weight_matrix
+
+def get_known_void_weight(r, x, y, void_weight_matrix, rcenters, xcenters, ycenters):
+    """Get void weights from matrix and void position.
+
+    """
+    N_grid_r, N_grid_x, N_grid_y = void_weight_matrix.shape
+    iy = np.abs(y[:,None].astype(np.float32) -\
+		ycenters[None,:].astype(np.float32)).argmin(axis=-1)
+    ix = np.abs(x[:,None].astype(np.float32) -\
+		xcenters[None,:].astype(np.float32)).argmin(axis=-1)
+    ir = np.abs(r[:, None].astype(np.float32) -\
+		rcenters[None,:].astype(np.float32)).argmin(axis=-1)
+    return void_weight_matrix[ir, ix, iy]
+if __name__ == '__main__':
+    if len(sys.argv) < 4:
+        sys.exit(f"ERROR: Unexpected number of arguments.\nUSAGE: {sys.argv[0]} RAW_VOID_DENS VOID_DENS VOID_CAT(S)")
+    raw_void_dens_fn = sys.argv[1]
+    void_dens_fn = sys.argv[2]
+    void_cat_fn_list = sys.argv[3:]
+    void_weight_nearest, void_weight_matrix =\
+		 get_known_void_weight_matrix(raw_void_dens_fn, void_dens_fn)
+    void_wt_coeff_fn = 'void_weights_c_of_r_real.dat' 
+    void_wt_coeff = np.loadtxt(void_wt_coeff_fn, dtype = np.float32)
+    rc, rw = edges_to_centers(radius_bins)
+    xc, xw = edges_to_centers(xedges)
+    yc, yw = edges_to_centers(yedges)
+    #batch_add_void_weights(void_cat_fn_list, get_weights_func=get_known_void_weight_matrix, void_weight_matrix=void_weight_matrix, rcenters=rc, xcenters=xc, ycenters=yc)
+    #batch_add_void_weights(void_cat_fn_list, void_weight_nearest,
+    #			 out_dir_suffix='_wt_nearest')
+
+    batch_add_void_weights(void_cat_fn_list, get_void_weights_interp, 
+			coeffs=void_wt_coeff, 
+			comp_function = lambda y, x: FUNCTION(y, x, N_grid=2500, Cmin = 0.8))
 