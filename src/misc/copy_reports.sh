81c150cb727f0ebae43e1789de62d784db35a887
Daniel Forero <daniel.forerosanchez@epfl.ch>
2020-04-07 11:49:42 +0200

    Fixed real void weight plot

--- src/misc/copy_reports.sh
+++ src/misc/copy_reports.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-find patchy_results/ -name '*.pdf' | xargs -I '{}' cp -v {} --parents reports/
-find results/ -name '*.pdf' | xargs -I '{}' cp -v {} --parents reports/
-find lrg_results/ -name '*.pdf' | xargs -I '{}' cp -v {} --parents reports/
+find patchy_results/ -name '*.pdf' | xargs -I '{}' cp -u -v {} --parents reports/
+find results/ -name '*.pdf' | xargs -I '{}' cp -u -v {} --parents reports/
+find lrg_results/ -name '*.pdf' | xargs -I '{}' cp -u -v {} --parents reports/
