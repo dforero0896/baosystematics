0c6c6546c268afed97d50da3eecc6c0e62eda42e
Daniel Forero <daniel.forerosanchez@epfl.ch>
2020-04-08 18:39:13 +0200

    Scaling radii on coeff plots and syncing binary data for plotting.

--- src/misc/copy_reports.sh
+++ src/misc/copy_reports.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 find patchy_results/ -name '*.pdf' | xargs -I '{}' cp -u -v {} --parents reports/
+find patchy_results/ -name '*.npy' | xargs -I '{}' cp -u -v {} --parents reports/
 find results/ -name '*.pdf' | xargs -I '{}' cp -u -v {} --parents reports/
 find lrg_results/ -name '*.pdf' | xargs -I '{}' cp -u -v {} --parents reports/
--- src/void_weights/plot_weight_fit_coeffs.py
+++ src/void_weights/plot_weight_fit_coeffs.py
@@ -4,13 +4,19 @@ import sys
 import os
 from cycler import cycler 
 import matplotlib.pyplot as plt
-
-def plot_weight_fit_coeffs(filename, ax, label='', **kwargs):
+from dotenv import load_dotenv
+load_dotenv()
+WORKDIR = os.environ.get('WORKDIR')
+SRC = os.environ.get('SRC')
+sys.path.append(f"{SRC}/simulate_systematics")
+from params import *
+def plot_weight_fit_coeffs(filename, ax, label='', ngal=None, **kwargs):
 
     data = np.loadtxt(filename)
-    
+    if ngal is not None: r_scaling = ngal**(1./3)
+    else: r_scaling=1
     for i in range(1,4):
-        ax.plot(data[:,0], data[:,i], label = label+' $c_%i$'%(i-1), **kwargs)
+        ax.plot(r_scaling * data[:,0], data[:,i], label = label+' $c_%i$'%(i-1), **kwargs)
         ax.set_ylabel('$c_i$', fontsize=15)
 
 
@@ -20,12 +26,16 @@ if __name__ == '__main__':
     ax.set_xlabel(r'$R$ [$h^{-1}$ Mpc]', fontsize=15)
     colorc = 'bgr'
     ax.set_prop_cycle(cycler(color=colorc))
-    plot_weight_fit_coeffs('void_weights_c_of_r_real.dat', ax=ax, label='real box1', ls = '-')
-    plot_weight_fit_coeffs('void_weights_c_of_r_redshift.dat', ax=ax, label='redshift box1', ls = '--')
-    plot_weight_fit_coeffs('void_weights_c_of_r_real_box5.dat', ax=ax, label='real box5', ls = ':', lw=3)
-    plot_weight_fit_coeffs('void_weights_c_of_r_redshift_box5.dat', ax=ax, label='redshift box5', ls = '-.')
+    n_box1 = 3.976980e-4
+    n_box5 = 1976125e-4
+    plot_weight_fit_coeffs('void_weights_c_of_r_real.dat', ax=ax, label='real box1', ls = '-', ngal = n_box1)
+    plot_weight_fit_coeffs('void_weights_c_of_r_redshift.dat', ax=ax, label='redshift box1', ls = '--', ngal=n_box1)
+    plot_weight_fit_coeffs('void_weights_c_of_r_real_box5.dat', ax=ax, label='real box5', ls = ':', lw=3, ngal=n_box5)
+    plot_weight_fit_coeffs('void_weights_c_of_r_redshift_box5.dat', ax=ax, label='redshift box5', ls = '-.', ngal=n_box5)
     ax.legend()
-    fitfig.savefig('r_vs_z_wtcoeffs.pdf', dpi=200)
+    odir = f"{WORKDIR}/patchy_results/box1/plots"
+    oname = f"{odir}/r_vs_z_wtcoeffs.pdf"
+    fitfig.savefig(oname, dpi=200)
     
 
 
