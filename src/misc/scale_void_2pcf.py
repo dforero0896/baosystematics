8d2174451fd4dc5159e5b432b129c8de902dea8d
Daniel Forero <daniel.forerosanchez@epfl.ch>
2020-04-09 16:55:53 +0200

    Other kind of rescaling shows no apparent improvement in peak.

--- src/misc/scale_void_2pcf.py
+++ src/misc/scale_void_2pcf.py
@@ -6,6 +6,25 @@ load_dotenv()
 WORKDIR=os.environ.get('WORKDIR')
 import numpy as np
 
+def get_rescaling_amplitude_match(syst, nosyst, smin=50, smax=80):
+    s = nosyst[:,0]
+    s_mask = (s>=50) & (s<=80)
+    pole_ratio = []
+    for i in [1, 3, 5]:
+        pole_ratio.append((nosyst[s_mask,i]/syst[s_mask,i]).mean())
+    
+    print(pole_ratio)
+    return pole_ratio
+
+def get_rescaling_baoerr_match(syst, nosyst, smin=82.5, smax=122.5):
+    s = nosyst[:,0]
+    s_mask = (s>=50) & (s<=80)
+    pole_ratio = []
+    for i in [2, 4, 6]:
+        pole_ratio.append((nosyst[s_mask,i]/syst[s_mask,i]).mean())
+    print(pole_ratio)
+    return pole_ratio
+
 if __name__ == '__main__':
     if len(sys.argv) != 3:
         sys.exit(f"USAGE: {sys.argv[0]} NOSYST_AVG_2PCF SYST_AVG_2PCF")
@@ -15,13 +34,12 @@ if __name__ == '__main__':
     
     nosyst = np.loadtxt(nosyst_fn)
     syst = np.loadtxt(syst_fn)
-    s = nosyst[:,0]
-    s_mask = (s>=50) & (s<=80)
-    for i in [1, 3, 5]:
-        pole_ratio = (nosyst[s_mask,i]/syst[s_mask,i]).mean()
-        syst[:,i]*=pole_ratio
+    get_rescaling_amplitude_match(syst, nosyst)
+    pole_ratio = get_rescaling_baoerr_match(syst, nosyst)
+    for j,i in enumerate([1, 3, 5]):
+        syst[:,i]*=pole_ratio[j]
     o,ext = os.path.splitext(syst_fn)
-    oname = f"{o}_rescaled{ext}"
+    oname = f"{o}_rescaled_baoerr{ext}"
     np.savetxt(oname, syst)
     print(f"==> Saved 2pcf in {oname}")
 
