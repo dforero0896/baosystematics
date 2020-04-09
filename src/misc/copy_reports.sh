18bb6b9938e49e059c1f6d134a8fa099d428890a
Daniel Forero <daniel.forerosanchez@epfl.ch>
2020-04-09 13:51:11 +0200

    Adding mock averages of 2pcfs

--- src/misc/copy_reports.sh
+++ src/misc/copy_reports.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 find patchy_results/ -name '*.pdf' | xargs -I '{}' cp -u -v {} --parents reports/
 find patchy_results/ -name '*.npy' | xargs -I '{}' cp -u -v {} --parents reports/
+find patchy_results/ -name '*mockavg*' | xargs -I '{}' cp -u -v {} --parents reports/
 find results/ -name '*.pdf' | xargs -I '{}' cp -u -v {} --parents reports/
 find lrg_results/ -name '*.pdf' | xargs -I '{}' cp -u -v {} --parents reports/
