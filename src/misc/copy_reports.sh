10c243b6777329252ed7b583ee07ef5f5d8031c1
Daniel Forero <daniel.forerosanchez@epfl.ch>
2020-04-22 21:27:37 +0200

    Data for EZ mocks 2pcfs

--- src/misc/copy_reports.sh
+++ src/misc/copy_reports.sh
@@ -4,5 +4,6 @@ find patchy_results/ -name '*.npy' -size -3G | xargs -I '{}' cp -u -v {} --paren
 find results/ -name '*.npy' -size -3G | xargs -I '{}' cp -u -v {} --parents reports/
 find patchy_results/ -name '*mockavg*' | xargs -I '{}' cp -u -v {} --parents reports/
 find results/ -name '*mockavg*ascii' | xargs -I '{}' cp -u -v {} --parents reports/
+find results/ -name '*xi0*mean*' | xargs -I '{}' cp -u -v {} --parents reports/
 find results/ -name '*.pdf' | xargs -I '{}' cp -u -v {} --parents reports/
 find lrg_results/ -name '*.pdf' | xargs -I '{}' cp -u -v {} --parents reports/
