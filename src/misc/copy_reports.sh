5c20051b1ac5ab7ad6ca7b364b02cbc910a187f4
Daniel Forero <daniel.forerosanchez@epfl.ch>
2020-04-22 09:21:38 +0200

    Radial gensity from allsyst EZ mocks

--- src/misc/copy_reports.sh
+++ src/misc/copy_reports.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 find patchy_results/ -wholename '*plots/*' -size -3G| xargs -I '{}' cp -u -v {} --parents reports/
 find patchy_results/ -name '*.npy' -size -3G | xargs -I '{}' cp -u -v {} --parents reports/
+find results/ -name '*.npy' -size -3G | xargs -I '{}' cp -u -v {} --parents reports/
 find patchy_results/ -name '*mockavg*' | xargs -I '{}' cp -u -v {} --parents reports/
 find results/ -name '*.pdf' | xargs -I '{}' cp -u -v {} --parents reports/
 find lrg_results/ -name '*.pdf' | xargs -I '{}' cp -u -v {} --parents reports/
