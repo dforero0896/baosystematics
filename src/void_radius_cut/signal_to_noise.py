e47b65f0229f15909328feba952004656a27d72c
Daniel Forero <daniel.forerosanchez@epfl.ch>
2020-05-07 17:22:02 +0200

    Jackknife estimate of the SNR.

--- src/void_radius_cut/signal_to_noise.py
+++ src/void_radius_cut/signal_to_noise.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python
 import numpy as np
+from astropy.stats import jackknife_resampling
+from astropy.stats import jackknife_stats
 import sys
 import os
 #import matplotlib.pyplot as plt
@@ -9,6 +11,8 @@ def read_inlist(inlist_fn):
         no_strip=f.readlines()
     [inlist.append(s.strip()) for s in no_strip]
     return inlist
+def signal_to_noise_ratio(arr):
+    return np.mean(arr)/np.std(arr)
 if __name__ == '__main__':
     if len(sys.argv)<2:
         sys.exit('ERROR: Unexpecte number of arguments.\nUSAGE: %s INPUT_FILE_LIST_1 [...INPUT_FILE_LIST_N]\n'%sys.argv[0])
@@ -20,6 +24,7 @@ if __name__ == '__main__':
     s_dr_1 = 117.5
     s_dr_2 = 122.5
     s_vals = [s_bao, s_dl_1, s_dl_2, s_dr_1, s_dr_2]
+    print(f"# SNR\tbias\tstderr\tconf_int_low\tconf_int_high\tsamples_dir\tn_samples")
     for ilist_fn in ilists_fn:
         ilist = read_inlist(ilist_fn)
         signal_arr=[]
@@ -31,5 +36,9 @@ if __name__ == '__main__':
             s_vals_id = np.array([np.where(s==val)[0] for val in s_vals])
             xi_vals = xi[s_vals_id]
             signal_arr.append(xi_vals[0] - np.mean(xi_vals[1:]))
-        SNR = np.mean(signal_arr)/np.std(signal_arr)
-        print(f"{SNR}\t{os.path.dirname(ilist[0])}\t{len(ilist)}")
+        #signal_resamples = jackknife_resampling(signal_arr)
+        signal_arr = np.array(signal_arr)
+        SNR, bias, stderr, conf_interval = jackknife_stats(signal_arr, signal_to_noise_ratio, 0.95)
+        print(f"{SNR}\t{bias}\t{stderr}\t{conf_interval[0]}\t{conf_interval[1]}\t{os.path.dirname(ilist[0])}\t{len(ilist)}")
+        #SNR = signal_to_noise_ratio(signal_arr)
+        #print(f"{SNR}\t{os.path.dirname(ilist[0])}\t{len(ilist)}")
