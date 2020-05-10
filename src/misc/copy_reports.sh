66c6cf5cfe1037a2a0ebba75061d92472c785bb5
Daniel Forero <daniel.forerosanchez@epfl.ch>
2020-05-10 16:20:07 +0200

    Void distribution for survey mocks

--- src/misc/copy_reports.sh
+++ src/misc/copy_reports.sh
@@ -3,6 +3,8 @@ find patchy_results/ -wholename '*plots/*' -size -3G| xargs -I '{}' cp -u -v {} 
 find patchy_results/ -name '*.npy' -size -3G | xargs -I '{}' cp -u -v {} --parents reports/
 find results/ -wholename '*plots/*' -size -3G| xargs -I '{}' cp -u -v {} --parents reports/
 find results/ -name '*.npy' -size -3G | xargs -I '{}' cp -u -v {} --parents reports/
+find lrg_results/ -wholename '*plots/*' -size -3G| xargs -I '{}' cp -u -v {} --parents reports/
+find lrg_results/ -name '*.npy' -size -3G | xargs -I '{}' cp -u -v {} --parents reports/
 find patchy_results/ -name '*mockavg*' | xargs -I '{}' cp -u -v {} --parents reports/
 find results/ -name '*mockavg*ascii' | xargs -I '{}' cp -u -v {} --parents reports/
 find results/ -name '*xi0*mean*' | xargs -I '{}' cp -u -v {} --parents reports/
