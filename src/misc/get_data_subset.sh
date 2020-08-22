036177ea4dfd0607395d90450afb7a87234dd432
Daniel Forero <daniel.forerosanchez@epfl.ch>
2020-08-22 17:25:51 +0200

    Copying subset of data script

--- src/misc/get_data_subset.sh
+++ src/misc/get_data_subset.sh
@@ -2,12 +2,13 @@
 SEED=1005638091
 side=$1
 if [[ "$side" == "S" ]]; then
-find ~+ -wholename "*nosyst*${SEED}*" | grep -v baofit > copy_to_local.dat
-sed -e "s/baosystematics/baosystematics\/./g" copyt_to_local.dat
+find ~+ -wholename "*nosyst*${SEED}*" | grep -v baofit > copy_to_local.dat.TMP
+sed -e "s/baosystematics/baosystematics\/./g" copy_to_local.dat.TMP > copy_to_local.dat
+rm -v copy_to_local.dat.TMP
 fi
 
 if [[ "$side" == "L" ]]; then
 DIR=${2:-"."}
-head -1 copy_to_local.dat | xargs -I'{}' rsync -av -e 'ssh -p 4022' dforero@localhost:{} --parents ${DIR}
+head -1 copy_to_local.dat | xargs -I'{}' rsync --relative -av -e 'ssh -p 4022' dforero@localhost:{} --parents ${DIR}
 fi
 
