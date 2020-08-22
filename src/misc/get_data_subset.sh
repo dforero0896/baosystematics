c550260fb80d6f50fbea58a7f30c890dbffade87
Daniel Forero <daniel.forerosanchez@epfl.ch>
2020-08-22 17:50:14 +0200

    Copying subset of data script

--- src/misc/get_data_subset.sh
+++ src/misc/get_data_subset.sh
@@ -10,7 +10,7 @@ elif [[ "$side" == "L" ]]; then
 DIR=${2:-"."}
 echo Running local side
 echo Saving in ${DIR}
-head -1 copy_to_local.dat | xargs -I'{}' rsync -P --relative -av -e 'ssh -p 4022' dforero@localhost:{} ${DIR}
+rsync -P --relative -av -e 'ssh -p 4022' --files-from=dforero@localhost:/hpcstorage/dforero/projects/baosystematics/copy_to_local.dat ${DIR}
 else
 echo Option not understood
 exit 1
