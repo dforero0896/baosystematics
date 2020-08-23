cdbcbf2fd1a1168a25c50425e052637914c60135
Daniel Forero <daniel.forerosanchez@epfl.ch>
2020-08-23 17:15:38 +0200

    Copy original cats

--- src/misc/get_data_subset.sh
+++ src/misc/get_data_subset.sh
@@ -3,14 +3,14 @@ SEED=1005638091
 side=$1
 if [[ "$side" == "S" ]]; then
 echo Running server side
-find ~+ -wholename "*nosyst*${SEED}*" | grep -v baofit > copy_to_local.dat.TMP
+find -L ~+ -wholename "*nosyst*${SEED}*" | grep -v baofit > copy_to_local.dat.TMP
 sed -e "s/baosystematics/baosystematics\/./g" copy_to_local.dat.TMP > copy_to_local.dat
 rm -v copy_to_local.dat.TMP
 elif [[ "$side" == "L" ]]; then
 DIR=${2:-"."}
 echo Running local side
 echo Saving in ${DIR}
-rsync -P --relative -av -e 'ssh -p 4022' --files-from=:/hpcstorage/dforero/projects/baosystematics/copy_to_local.dat dforero@localhost:/ ${DIR}
+rsync -P --ignore-existing --relative -av -e 'ssh -p 4022' --files-from=:/hpcstorage/dforero/projects/baosystematics/copy_to_local.dat dforero@localhost:/ ${DIR}
 else
 echo Option not understood
 exit 1
