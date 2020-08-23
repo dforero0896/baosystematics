c93c363277a579ac7e3aec228a3de6493aa1eb3a
Daniel Forero <daniel.forerosanchez@epfl.ch>
2020-08-23 17:53:27 +0200

    Posec nbodykit script

--- src/powspec/powspec_nbodykit.py
+++ src/powspec/powspec_nbodykit.py
@@ -33,6 +33,7 @@ if __name__=='__main__':
     parser.add_argument('-ns', '--n_s', help="Tilt of primordial power spectrum. Default 0.96", default = 0.96)
     parser.add_argument('-rsd', '--rsd', help="Whether to compute the redshift space catalog. Default 0.", default=0)
     parser.add_argument('-bs', '--box-size', help="Box size of the simulation box. Default 2500 Mpc/h.", default=2500)
+    parser.add_argument('-o', '--out', help="Output directory. Default: . ", default='.')
     parsed=parser.parse_args()
     args = vars(parsed)
     print(args)
@@ -47,5 +48,31 @@ if __name__=='__main__':
     catalog['VelocityOffset'] = (1 + redshift) / (100 * cosmo.efunc(redshift))
     if bool(int(args['rsd'])): catalog['RSDPosition'] = catalog['Position'] + catalog['VelocityOffset'] * line_of_sight
     mesh = catalog.to_mesh(resampler='cic', Nmesh=256, compensated=True, position='Position', BoxSize=args['box_size'])
+    r = FFTPower(mesh, mode='1d', dk=0.005, kmin=0.01) 
+    Pk = r.power
+    print(Pk)
+    for k in Pk.attrs:
+        print("%s = %s" %(k, str(Pk.attrs[k])))
+    
+    # print the shot noise subtracted P(k)
+    plt.loglog(Pk['k'], Pk['power'].real - Pk.attrs['shotnoise'], label="1D")
 
- 
+    r = FFTPower(mesh, mode='2d', dk=0.005, kmin=0.01, Nmu=5, los=[0,0,1], poles=[0])
+    poles = r.poles
+    for ell in [0]:
+        label = r'$\ell=%d$' % (ell)
+        P = poles['power_%d' %ell].real
+        if ell == 0: P = P - poles.attrs['shotnoise']
+        plt.loglog(poles['k'], P, label=label)    
+    plt.legend(loc=0)
+    plt.xlabel(r"$k$ [$h \ \mathrm{Mpc}^{-1}$]")
+    plt.ylabel(r"$k \ P_\ell$ [$h^{-2} \mathrm{Mpc}^2$]")
+    plt.xlim(0.01, 0.6)
+
+
+    Pk1d=np.c_[Pk['k'], Pk['power'].real-Pk.attrs['shotnoise'])
+    np.savetxt(f"{args['out']}/powspec1d_{os.path.basename(args['filename'])}", Pk1d)
+    Pk1d=np.c_[poles['k'], poles['power_0'].real-poles.attrs['shotnoise'])
+    np.savetxt(f"{args['out']}/powspec2d_ell0_{os.path.basename(args['filename'])}", Pk1d)
+    plt.gcf()
+    plt.savefig(f"{args['out']}/powspec_{os.path.basename(args['filename'])}.pdf", dpi=300, rasterize=True)
