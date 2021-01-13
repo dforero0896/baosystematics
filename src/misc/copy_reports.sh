dc2c0cd129e75366d6bda58aab470554cb0a83c8
Daniel Forero <daniel.forerosanchez@epfl.ch>
2021-01-13 10:46:34 +0100

    Average 2PCFs of uniformly downsampled boxes

--- src/misc/copy_reports.sh
+++ src/misc/copy_reports.sh
@@ -1,21 +1,22 @@
-#!/bin/bash
-#find abacus_results/ -name '*.npy' -size -3G | xargs -I '{}' cp -u -v {} --parents reports/
-#find abacus_results/ -wholename '*/avg*' -size -3G | xargs -I '{}' cp -u -v {} --parents reports/
-find patchy_results/ -wholename '*plots/*' -size -3G| xargs -I '{}' cp -u -v {} --parents reports/
-#find patchy_results/ -name '*.npy' -size -3G | xargs -I '{}' cp -u -v {} --parents reports/
-find patchy_results/ -wholename '*baofit/avg*/*' -size -3G | xargs -I '{}' cp -u -v {} --parents reports/
-find patchy_results/ -wholename '*baofit/*/alpha_samples.dat' -size -3G | xargs -I '{}' cp -u -v {} --parents reports/
-find patchy_recon/ -wholename '*plots/*' -size -3G| xargs -I '{}' cp -u -v {} --parents reports/
-#find patchy_recon/ -name '*.npy' -size -3G | xargs -I '{}' cp -u -v {} --parents reports/
-find patchy_recon/ -wholename '*baofit/avg*/*' -size -3G | xargs -I '{}' cp -u -v {} --parents reports/
-find patchy_recon_nods/ -wholename '*plots/*' -size -3G| xargs -I '{}' cp -u -v {} --parents reports/
-#find patchy_recon_nods/ -name '*.npy' -size -3G | xargs -I '{}' cp -u -v {} --parents reports/
-find patchy_recon_nods/ -wholename '*baofit/avg*/*' -size -3G | xargs -I '{}' cp -u -v {} --parents reports/
-#find patchy_linhalo/ -name '*.npy' -size -3G | xargs -I '{}' cp -u -v {} --parents reports/
-find lrg_results/ -wholename '*plots/*' -size -3G| xargs -I '{}' cp -u -v {} --parents reports/
-find lrg_results/ -name '*.npy' -size -3G | xargs -I '{}' cp -u -v {} --parents reports/
-find patchy_results/ -name '*mockavg*' | xargs -I '{}' cp -u -v {} --parents reports/
-find results/ -name '*mockavg*ascii' | xargs -I '{}' cp -u -v {} --parents reports/
-find results/ -name '*xi0*mean*' | xargs -I '{}' cp -u -v {} --parents reports/
-find results/ -name '*.pdf' | xargs -I '{}' cp -u -v {} --parents reports/
-find lrg_results/ -name '*.pdf' | xargs -I '{}' cp -u -v {} --parents reports/
+#!/bin/bash &
+#find abacus_results/ -name '*.npy' -size -3G | xargs -I '{}' cp -u -v {} --parents reports/ &
+#find abacus_results/ -wholename '*/avg*' -size -3G | xargs -I '{}' cp -u -v {} --parents reports/ &
+find patchy_results/ -wholename '*plots/*' -size -3G| xargs -I '{}' cp -u -v {} --parents reports/ &
+#find patchy_results/ -name '*.npy' -size -3G | xargs -I '{}' cp -u -v {} --parents reports/ &
+find patchy_results/ -wholename '*baofit/avg*/*' -size -3G | xargs -I '{}' cp -u -v {} --parents reports/ &
+find patchy_results/ -wholename '*baofit/*/alpha_samples.dat' -size -3G | xargs -I '{}' cp -u -v {} --parents reports/ &
+find patchy_recon/ -wholename '*plots/*' -size -3G| xargs -I '{}' cp -u -v {} --parents reports/ &
+#find patchy_recon/ -name '*.npy' -size -3G | xargs -I '{}' cp -u -v {} --parents reports/ &
+find patchy_recon/ -wholename '*baofit/avg*/*' -size -3G | xargs -I '{}' cp -u -v {} --parents reports/ &
+find patchy_recon_nods/ -wholename '*plots/*' -size -3G| xargs -I '{}' cp -u -v {} --parents reports/ &
+#find patchy_recon_nods/ -name '*.npy' -size -3G | xargs -I '{}' cp -u -v {} --parents reports/ &
+find patchy_recon_nods/ -wholename '*baofit/avg*/*' -size -3G | xargs -I '{}' cp -u -v {} --parents reports/ &
+#find patchy_linhalo/ -name '*.npy' -size -3G | xargs -I '{}' cp -u -v {} --parents reports/ &
+find lrg_results/ -wholename '*plots/*' -size -3G| xargs -I '{}' cp -u -v {} --parents reports/ &
+find lrg_results/ -name '*.npy' -size -3G | xargs -I '{}' cp -u -v {} --parents reports/ &
+find patchy_results/ -name '*mockavg*' | xargs -I '{}' cp -u -v {} --parents reports/ &
+find patchy_recon_nods/ -name '*mockavg*' | xargs -I '{}' cp -u -v {} --parents reports/ &
+find results/ -name '*mockavg*ascii' | xargs -I '{}' cp -u -v {} --parents reports/ &
+find results/ -name '*xi0*mean*' | xargs -I '{}' cp -u -v {} --parents reports/ &
+find results/ -name '*.pdf' | xargs -I '{}' cp -u -v {} --parents reports/ &
+find lrg_results/ -name '*.pdf' | xargs -I '{}' cp -u -v {} --parents reports/ &
